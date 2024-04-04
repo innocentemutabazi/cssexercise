ae982d4d24398771691c1031859be10365dd8bd8
innocentemutabazi <innocentemutabazi@gmail.com>
2024-04-04 17:59:44 +0200

    Updated the fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        Noto:['Noto Sans'],
+      }
+    },
   },
   plugins: [],
 }
